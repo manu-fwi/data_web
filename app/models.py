eb945b3583828a3d6b670396786d59f55550e0fe
manu <eallaud@gmail.com>
2022-01-28 19:10:33 -0400

    Define 2 new db classes that handle the dashboards and their graphs

--- app/models.py
+++ app/models.py
@@ -7,9 +7,10 @@ from app import db
 # that we must add the time of reception (up to ms); this is implicitly done for the
 # VALUE format
 # fields: comma separated list of fields name
-# for JSON and CSV format one field MUST BE "emission_date!format" "emission_time!format!timezone" or "emission_datetime!format!timezone" indicating the time of emission (note the double quotes)
+# for JSON and CSV format one field MUST BE "emission_datetime!format!timezone" indicating the time of emission (note the double quotes)
 # only the emission_date and time field is enclosed in double quotes (")
-#for CSV format: no comma can be present in the format fields and in the payload (that would mess up the parsing the payload
+#for CSV format: no comma can be present in the format fields and in the payload (that would mess up the parsing
+#of the payload
 # format being codified as for strptime function (no timezone must be included in the format)
 # if no timezone is provided, UTC is assumed
 # all times/dates will be stored in UTC
@@ -44,3 +45,33 @@ class db_updates(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     stream_id = db.Column(db.Integer,db.ForeignKey('data_streams.id'))
     data_stream = db.relationship('db_data_streams',foreign_keys=stream_id)
+
+# Dashboard related models
+class db_dashboard(db.Model):
+    __tablename__ = "dashboards"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200),index=True)
+
+# describes a graph (what data feeds, type,...)
+
+class db_graph(db.Model):
+    __tablename__ = "graphs"
+    id = db.Column(db.Integer, primary_key=True)
+    #name: used to find the html object to put it into
+    name = db.Column(db.String(50),index=True)
+    #dashboard it belongs to
+    dashboard_id = db.Column(db.Integer,db.ForeignKey('dashboards.id'))
+    #Position and size in a string in x,y,w,h format
+    #can be Null
+    rect = db.Column(db.String(20))
+    #data to describe the graph
+    # graph_type: string, can be: lines FIXME
+    graph_type = db.Column(db.String(50))
+    # data_xy: json as a string {"x":data stream name,"y":[data stream names]}
+    # data stream name is: "name[field]", if field is not present the first field which is not a date is taken
+    data_xy = db.Column(db.String(200))
+    # options: json as a string FIXME (axes, hover style,...)
+    options = db.Column(db.String(300))
+    
+        
+
